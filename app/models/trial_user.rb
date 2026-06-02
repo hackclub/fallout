02b1745844be2e7ae4b9cd1e7fcb2f60fc34ebbb
Tanishq Goyal <146565444+TaniWanKenobi@users.noreply.github.com>
2026-06-02 19:35:23 -0400

    Remove hacker_value annotation from trial_user

--- app/models/trial_user.rb
+++ app/models/trial_user.rb
@@ -1,4 +1,4 @@
-﻿# == Schema Information
+# == Schema Information
 #
 # Table name: users
 #
@@ -15,7 +15,6 @@
 #  excluded_from_reviewer_suggestions :boolean          default(FALSE), not null
 #  first_name                         :string
 #  gold_balance                       :integer          default(0), not null
-#  hacker_value                       :integer          default(0), not null
 #  has_hca_address                    :boolean          default(FALSE), not null
 #  hca_token                          :text
 #  hcb_email                          :string
@@ -32,7 +31,6 @@
 #  streak_freezes                     :integer          default(1), not null
 #  streak_in_app_notifications        :boolean          default(TRUE), not null
 #  streak_slack_notifications         :boolean          default(TRUE), not null
-#  summit_rsvp                        :string
 #  timezone                           :string           not null
 #  type                               :string
 #  verification_status                :string
