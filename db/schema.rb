0f3930084e80fdf226a7ffd7eda08ecdbf366759
Sam Liu <sam@samliu.dev>
2026-06-04 13:03:36 -0400

    Update schema and fixtures for project model attributes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_28_131000) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_04_173000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_trgm"
@@ -897,6 +897,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_28_131000) do
     t.datetime "sent_at"
     t.integer "soup_campaign_recipients_count", default: 0, null: false
     t.integer "status", default: 0, null: false
+    t.text "target_query"
+    t.integer "target_user_ids", default: [], null: false, array: true
     t.string "unsubscribe_label", default: "Important program related announcement | Unsubscribe", null: false
     t.string "unsubscribe_token", null: false
     t.datetime "updated_at", null: false
