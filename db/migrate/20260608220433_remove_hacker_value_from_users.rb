34720f1d9f1c3170f421a3d8612ec24423eca1fc
Sam Liu <sam@samliu.dev>
2026-06-09 15:31:25 -0400

    Add if_exists option to remove_column for hacker_value

--- db/migrate/20260608220433_remove_hacker_value_from_users.rb
+++ db/migrate/20260608220433_remove_hacker_value_from_users.rb
@@ -1,5 +1,5 @@
 class RemoveHackerValueFromUsers < ActiveRecord::Migration[8.1]
   def change
-    remove_column :users, :hacker_value, :integer, default: 0, null: false
+    remove_column :users, :hacker_value, :integer, default: 0, null: false, if_exists: true
   end
 end
